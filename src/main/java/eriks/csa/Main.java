f9b7de6306644f2de016884a1986a7f52ea65421
Erik Simões Oliveira <eriksimoes.oliveira@gmail.com>
2024-08-05 16:02:25 -0600

    Initial commit

--- src/main/java/eriks/csa/Main.java
+++ src/main/java/eriks/csa/Main.java
@@ -7,11 +7,11 @@ import io.quarkus.runtime.annotations.QuarkusMain;
 @QuarkusMain
 public class Main {
     public static void main(String[] args) {
-        String databaseUrl = System.getenv("DATABASE_URL");
-        if (databaseUrl != null) {
-            String jdbcUrl = DatabaseUrlConverter.convertHerokuDatabaseUrl(databaseUrl);
-            System.setProperty("JDBC_DATABASE_URL", jdbcUrl);
-        }
+//        String databaseUrl = System.getenv("DATABASE_URL");
+//        if (databaseUrl != null) {
+//            String jdbcUrl = DatabaseUrlConverter.convertHerokuDatabaseUrl(databaseUrl);
+//            System.setProperty("JDBC_DATABASE_URL", jdbcUrl);
+//        }
         Quarkus.run(args);
     }
 }
